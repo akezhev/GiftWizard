242019050701665b715295c9c35781ed5eaec24a
akezhev <akezh@mail.ru>
2026-04-26 19:23:10 +0300

    added a new file index.js

---
+++ backend/src/models/index.js
@@ -0,0 +1,11 @@
+const User = require('./User');
+const QuizResult = require('./QuizResult');
+const Gift = require('./Gift');
+const Marketplace = require('./Marketplace');
+
+module.exports = {
+  User,
+  QuizResult,
+  Gift,
+  Marketplace,
+};
